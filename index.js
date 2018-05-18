22de9667a9920fdbfb99dda5c628272e25977e78
Denise Pendleton <dpendl1@hotmail.com>
2018-05-18 20:52:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const currentUser = "Grace Hopper";
 let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
 
 
-let excitedWelcomeMessage = "WELCOME TO FLATBOOK, "
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser
